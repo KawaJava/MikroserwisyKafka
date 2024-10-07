36f128a6044d16fa1a3e9ec3793200947199f713
kawajava <kawajava0550@gmail.com>
2024-10-07 12:54:14 +0200

    Branch to Pull Request for Skillforge

---
+++ Konsument/src/test/java/kawajava/github/io/konsument/KonsumentApplicationTests.java
@@ -0,0 +1,13 @@
+package kawajava.github.io.konsument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonsumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
