0034ad9dcedad7903709228a1d3d5cf561307e5c
kawajava <kawajava0550@gmail.com>
2024-10-07 12:48:55 +0200

    base project

--- Generator/src/main/java/kawajava/github/io/generator/order/model/OrderStatus.java
+++
@@ -1,4 +0,0 @@
-package kawajava.github.io.generator.order.model;
-
-public enum OrderStatus {NEW, PAID, COMPLETED, CANCELED
-}
--- Generator/src/main/java/kawajava/github/io/generator/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package kawajava.github.io.generator.order.repository;
-
-import kawajava.github.io.generator.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
